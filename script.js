91f5249e215712069846779092db1f0574d73d97
sabarigv <94048123+sabarigv@users.noreply.github.com>
2022-10-03 15:33:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+{
+    setTimeout(function (){
+      document.getElementById("time").innerText = "10";
+      setTimeout(function (){
+        document.getElementById("time").innerText = "9";
+        setTimeout(function (){
+          document.getElementById("time").innerText = "8";
+          setTimeout(function (){
+            document.getElementById("time").innerText = "7";
+            setTimeout(function (){
+              document.getElementById("time").innerText = "6";
+              setTimeout(function (){
+                document.getElementById("time").innerText = "5";
+                setTimeout(function (){
+                  document.getElementById("time").innerText = "4";
+                  setTimeout(function (){
+                    document.getElementById("time").innerText = "3";
+                    setTimeout(function (){
+                      document.getElementById("time").innerText = "2";
+                      setTimeout(function (){
+                        document.getElementById("time").innerText = "1";
+                        setTimeout(function (){
+                          document.getElementById("time").innerText = "Happy Independence Day ";
+                        }, 1000)
+                      }, 1000)
+                    }, 1000)
+                  }, 1000)
+                }, 1000)
+              }, 1000)
+            }, 1000)
+          }, 1000)
+        }, 1000)
+      }, 1000)
+    }, 1000) 
+  }
